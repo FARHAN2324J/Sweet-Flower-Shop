b2462d57e16f4ccff7fb621eb6664961b45d260e
farhan <farhanfadayi@gmail.com>
2025-12-13 13:56:30 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from "./pages/Home/components/Hero"
+
+function App() {
+
+  return (
+    <>
+    <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <header>
+      <h1 className="Display">hello</h1>
+    </header>
+  );
+};
+
+export default Hero;
