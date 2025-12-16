6ad86d8e3a8ef040ca2e2ed4c7014d0723127ef1
farhan <farhanfadayi@gmail.com>
2025-12-16 22:03:18 +0330

    bug GSAP

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,4 +1,3 @@
-import WorkWithUs from "../Home/components/WorkWithUs";
 import FlowersGallery from "./components/FlowersGallery";
 import Services from "./components/Services";
 
@@ -7,7 +6,6 @@ const Gallery = () => {
     <>
       <FlowersGallery />
       <Services />
-      <WorkWithUs />
     </>
   );
 };
